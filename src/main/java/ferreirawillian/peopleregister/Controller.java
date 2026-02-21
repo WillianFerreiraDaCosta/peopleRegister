1c1629a8d4624c16d20b17f16b890f789e386892
Willian F <ferreiradacostawillian424@gmail.com>
2026-02-21 10:39:26 -0300

    Criado Controller

---
+++ src/main/java/ferreirawillian/peopleregister/Controller.java
@@ -0,0 +1,15 @@
+package ferreirawillian.peopleregister;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Ola, primeira mensagem nessa rota!!";
+    }
+}
