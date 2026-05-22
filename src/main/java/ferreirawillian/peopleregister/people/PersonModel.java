c9eeecf0ac62f99f6339f6493bc72884efb350fb
Willian Ferreira da Costa <ferreiradacostawillian424@gmail.com>
2026-05-22 06:32:35 -0300

    feat: Include Lombok annotations in the PeopleModel and TaskModel classes

--- src/main/java/ferreirawillian/peopleregister/people/PersonModel.java
+++ src/main/java/ferreirawillian/peopleregister/people/PersonModel.java
@@ -2,106 +2,34 @@ package ferreirawillian.peopleregister.people;
 
 import ferreirawillian.peopleregister.task.TaskModel;
 import jakarta.persistence.*;
+import lombok.*;
+
 import java.util.List;
 
 //transforma em uma entidade(tabela) no banco de dados
 //JPA = Java Persistence API
 @Entity
 @Table(name = "tb_people")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PersonModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String cpf;
+
     private String dateOfBirth;
+
     private String email;
+
     private String job;
+
     @OneToMany(mappedBy = "person")//Uma pessoa com muitas missoes
     private List<TaskModel> tasks;
-    public PersonModel() {}
 
-    /*
-    *
-    *
-    *
-    *
-    *
-    *
-    *
-    *
-    *
-    *
-    * arrumar os construtores, aula 7, 18:30
-    *
-    *
-    *
-    *
-    *
-    *
-    *
-    *
-    *
-    *
-    *
-    *
-    *
-    *
-    * */
-
-    public PersonModel(String cpf, String dateOfBirth, String email, String job, String name) {
-        setCpf(cpf);
-        setDateOfBirth(dateOfBirth);
-        setEmail(email);
-        setJob(job);
-        setName(name);
-    }
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public String getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getJob() {
-        return job;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public void setDateOfBirth(String dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setJob(String job) {
-        this.job = job;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
